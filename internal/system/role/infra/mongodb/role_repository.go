a84defbf4dcacc04d97a64d258d12d10463e4148
Mrokoo <1874756925@qq.com>
2023-03-27 20:59:01 +0800

    增加测试

--- internal/system/role/infra/mongodb/role_repository.go
+++ internal/system/role/infra/mongodb/role_repository.go
@@ -57,8 +57,7 @@ func (r *MongoRepository) Update(role *domain.Role) error {
 }
 
 func (r *MongoRepository) Delete(name string) error {
-	ok, err := r.e.RemoveFilteredGroupingPolicy(0, name)
-	println(ok)
+	_, err := r.e.RemoveFilteredPolicy(0, name)
 	if err != nil {
 		return err
 	}
@@ -74,14 +73,39 @@ func (r *MongoRepository) FindByName(name string) (*domain.Role, error) {
 	}, nil
 }
 
-// func (r *MongoRepository) FindaAll() ([]*domain.Role, error) {
+func (r *MongoRepository) FindaAll() ([]*domain.Role, error) {
+	var allrole []*domain.Role
+	allSubjects := r.e.GetAllSubjects()
+	for _, v := range allSubjects {
+		role, err := r.FindByName(v)
+		if err != nil {
+			return nil, err
+		}
+		allrole = append(allrole, role)
+	}
 
-// }
+	return allrole, nil
+}
 
 func toRole(p [][]string) []domain.PermissionItem {
 	var pp []domain.PermissionItem
 	for _, v := range p {
 		pp = append(pp, v[1])
 	}
-	return pp
+	return removeRepByMap(pp)
+}
+
+// slice去重
+func removeRepByMap(slc []string) []string {
+	result := []string{}         //存放返回的不重复切片
+	tempMap := map[string]byte{} // 存放不重复主键
+	for _, e := range slc {
+		l := len(tempMap)
+		tempMap[e] = 0 //当e存在于tempMap中时，再次添加是添加不进去的，，因为key不允许重复
+		//如果上一行添加成功，那么长度发生变化且此时元素一定不重复
+		if len(tempMap) != l { // 加入map后，map长度变化，则元素不重复
+			result = append(result, e) //当元素不重复时，将元素添加到切片result中
+		}
+	}
+	return result
 }
--- internal/system/role/infra/mongodb/role_repository_test.go
+++ internal/system/role/infra/mongodb/role_repository_test.go
@@ -5,13 +5,14 @@ import (
 	"testing"
 
 	"github.com/mrokoo/goERP/internal/system/role/domain"
+	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var r *MongoRepository
 
-func Test_MongoRepository_Save(t *testing.T) {
+func TestMongoRepository_Save(t *testing.T) {
 	connectionString := "mongodb://localhost:27017/"
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(connectionString))
 	if err != nil {
@@ -29,7 +30,60 @@ func Test_MongoRepository_Save(t *testing.T) {
 	}
 }
 
-func Test_MongoRepository_Delete(t *testing.T) {
+func TestMongoRepository_Update(t *testing.T) {
+	connectionString := "mongodb://localhost:27017/"
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(connectionString))
+	if err != nil {
+		panic(err)
+	}
+	r = NewMongoRepository(client)
+	role := &domain.Role{
+		Name:       "admin",
+		Permission: []domain.PermissionItem{"account", "customer"},
+	}
+	err = r.Update(role)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestMongoRepository_FindByName(t *testing.T) {
+	connectionString := "mongodb://localhost:27017/"
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(connectionString))
+	if err != nil {
+		panic(err)
+	}
+	r = NewMongoRepository(client)
+	role, err := r.FindByName("admin")
+	if err != nil {
+		t.Error(err)
+	}
+	role2 := &domain.Role{
+		Name:       "admin",
+		Permission: []domain.PermissionItem{"account", "customer"},
+	}
+	assert.Equal(t, *role2, *role)
+}
+
+func TestMongoRepository_FindAll(t *testing.T) {
+	connectionString := "mongodb://localhost:27017/"
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(connectionString))
+	if err != nil {
+		panic(err)
+	}
+	r = NewMongoRepository(client)
+	role2 := &domain.Role{
+		Name:       "admin",
+		Permission: []domain.PermissionItem{"account", "customer"},
+	}
+	result, err := r.FindaAll()
+	if err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, *role2, *result[0])
+}
+
+func TestMongoRepository_Delete(t *testing.T) {
 	connectionString := "mongodb://localhost:27017/"
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(connectionString))
 	if err != nil {
